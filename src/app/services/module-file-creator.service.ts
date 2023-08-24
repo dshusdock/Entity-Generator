c61ed63b3a670c176bb2f0a68f0c219faab96263
dshusdock <dshusdock@gmail.com>
2023-08-24 20:22:46 +0000

    work on mod info choices

--- src/app/services/module-file-creator.service.ts
+++ src/app/services/module-file-creator.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { EntityInfoService } from './entity-info.service';
+import { AppInfoService } from './app-info.service';
 
 const tmpltVals = {
     name: '',
@@ -10,14 +11,30 @@ const tmpltVals = {
     providedIn: 'root'
 })
 export class ModuleFileCreatorService {
+    bitMask=0;
     
-
-    constructor(private readonly entityInfoSvc: EntityInfoService) { }
+    constructor(private readonly entityInfoSvc: EntityInfoService,
+                private readonly appInfoSvc: AppInfoService) { }
 
     generateFile(): Blob {
         tmpltVals.name = this.entityInfoSvc.entityClassName;
         tmpltVals.lowercaseName = tmpltVals.name.toLocaleLowerCase();
-        const file = new Blob([moduleTmplt2(1)], { type: "text/plain" });
+
+        if (this.appInfoSvc.abstractRepositorySupport) {
+            this.bitMask = this.bitMask | 4;
+        }
+        
+        if (this.appInfoSvc.graphQLSupport) {
+            this.bitMask = this.bitMask | 2;
+        }
+
+        if (this.appInfoSvc.mongoDBSupport) {
+            this.bitMask = this.bitMask | 1
+        }
+
+        console.log("bitmask = " + this.bitMask);
+
+        const file = new Blob([moduleTmplt2(this.bitMask)], { type: "text/plain" });
         return file;
 
     }
@@ -49,25 +66,30 @@ function moduleTmplt() {
 
 function moduleTmplt2(bitMask: number) {
     let finalStr = "";
-    let baseHeaderStr = `    import { Module } from '@nestjs/common';
+
+    const HEADER = {
+        base: `    import { Module } from '@nestjs/common';
     import { ${tmpltVals.name}Service } from './${tmpltVals.lowercaseName}.service';
     import { ${tmpltVals.name}Controller } from './${tmpltVals.lowercaseName}.controller';
     import { ${tmpltVals.name} } from './entities/${tmpltVals.lowercaseName}.model';    
-    `;
-
-    let mongooseHdrStr = `import { MongooseModule } from '@nestjs/mongoose';
+    `,
+        mongoose: `import { MongooseModule } from '@nestjs/mongoose';
     import { ${tmpltVals.name}Schema } from './entities/${tmpltVals.lowercaseName}.schema';
     import { ${tmpltVals.name}Repository } from './${tmpltVals.lowercaseName}.repository';
-    `;
-
-    let graphQLHdrStr = `import { ${tmpltVals.name}Resolver } from './${tmpltVals.lowercaseName}.resolver';
-    `;
-
-    finalStr = baseHeaderStr;
-
-    if( bitMask & 1) {
-        finalStr = finalStr + mongooseHdrStr;
+    `,
+        graphql: `import { ${tmpltVals.name}Resolver } from './${tmpltVals.lowercaseName}.resolver';
+    `,
+        abstractRepo: ``
     }
 
+    finalStr = HEADER.base;
+
+    // Header Section
+    if (bitMask&1) {finalStr = finalStr + HEADER.mongoose;}
+    if (bitMask&2) {finalStr = finalStr + HEADER.graphql;}
+    if (bitMask&4) {finalStr = finalStr + HEADER.abstractRepo;}
+    
     return finalStr;
 }
+
+
